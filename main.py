4a9bacf6e4258e82a23b706d83804678256107a5
Illia Tishchenko <102582375+illiatishchenko98@users.noreply.github.com>
2023-04-05 21:55:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,150 @@
+# first = [10, '9', True, 11.5, {}]
+
+
+# print(first.pop(2))
+
+# print(len(first))
+
+# print(first[:: -1])
+
+
+# second = [24, 25]
+
+# result = first + second
+
+# print(result)
+
+
+# first = [10, '9', True, 11.5, {}]
+
+
+# second = [24, 25]
+
+
+# result = first + second
+
+
+# merged = first.__add__(second)
+
+# print(merged)
+
+
+# my_car = {
+#     "model": "Mercedes",
+#     'year': "2023",
+#     "cost": "150_000"
+
+# }
+
+
+# print(my_car.get('qt', 'Error'))
+
+# print(my_car['model'])
+
+
+# my_car["cost"] = '250_000'
+
+# print(my_car["cost"])
+
+# my_car['AMG'] = 'yes'
+
+# print(my_car)
+
+# del my_car['AMG']
+# print(my_car)
+
+
+# my_disk = {}
+
+# print(id(my_disk))
+# print(type(my_disk))
+
+# my_disk['brand'] = 'Samsung'
+# my_disk['price'] = 80
+
+# print(my_disk)
+
+
+# empty_dict = {}
+
+
+# k1 = input('write a 1 key:')
+# v1 = input('write a 1 value:')
+
+
+# empty_dict[k1] = v1
+
+
+# k2 = input('write a 2 key:')
+# v2 = input('write a 2 value:')
+
+
+# empty_dict[k2] = v2
+
+
+# k3 = input('write a 3 key:')
+# v3 = input('write a 3 value:')
+
+
+# empty_dict[k3] = v3
+
+
+# print(empty_dict)
+
+
+# my_tup = (10, 2, 14, 58)
+
+# my_tup_list = list(my_tup)
+
+# my_tup_list.append(24)
+
+
+# tuple(my_tup_list)
+# print(my_tup_list)
+
+
+# my_set = {'abc', 'd', 'f', 'y'}
+
+# other_set = {'a', 'f', 'd'}
+
+# print(my_set.intersection(other_set))
+
+# print(my_set.union(other_set))
+
+
+# print(my_set & other_set)
+
+
+# a = {11, 24, 25, 121, 48}
+
+# b = {24, 343, 232, 11, 48}
+
+# ab = a & b
+
+# abc = list(ab)
+# print(list(ab))
+# print(type(abc))
+
+
+# my_range = range(5)
+
+# for n in my_range:
+#     print(n)
+
+
+# my_range = range(5)
+
+# print(dir(my_range))
+
+# first = ['asd', 'asdas', 'aeefe']
+# second = [15, 232, 4354]
+
+# merge = zip(first, second)
+# print(list(merge))
+
+
+# def first():
+#     pass
+
+
+# print(first())
